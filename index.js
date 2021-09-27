406c93dfbb0195a5b8c2c7e1dd001698693c015c
Andres <ace_vedo@hotmail.com>
2021-09-27 12:39:34 -0500

    branch testing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let message = '';
-let answer = '';
+const answer1 = 'En estos momentos no tenemos habitaciones disponibles, si resultan vacantes le informaremos inmediatamente';
+const answer2 = 'Por favor introduzcxa la cantidad de personas que desean hospedarse'
+let waiting = []
 
